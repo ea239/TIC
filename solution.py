f0be3aa675c860e9feb03e32a3b714b1b1633587
ea239 <62815042+ea239@users.noreply.github.com>
2025-05-24 14:47:31 -0400

    Challenge

--- solution.py
+++ solution.py
@@ -24,6 +24,32 @@ imu = IMU(robot)
 logging = Logging(robot)
 lidar = Lidar(robot)
 
+def auto_avoid_obstacle(lidar, 
+                        control,
+                        robot, 
+                        trigger_distance=0.4, 
+                        safe_back_speed=0.1, back_duration=2.0, cone_angle=20):
+    rclpy.spin_once(robot, timeout_sec=0.01)
+    laserMsg = lidar.checkScan()
+    if laserMsg is None:
+        return False
+
+    dis, angle = lidar.detect_obstacle_in_cone(laserMsg, trigger_distance, center=0, offset_angle=cone_angle)
+    
+    if dis != -1:
+        print(f"dis = {dis:.2f} m, angle = {angle:.2f}°")
+
+        control.stop_keyboard_control()
+        control.set_cmd_vel(0.0, 0.0, 0)
+        time.sleep(1)
+
+        control.set_cmd_vel(-safe_back_speed, 0.0, back_duration)
+        control.start_keyboard_control()
+        time.sleep(0.1)
+        return True
+
+    return False
+
 def handle_stop_sign_and_apriltag(camera, control, robot,
                                    stop_duration=2, cooldown_after_stop=30,
                                    check_interval=5):
@@ -79,17 +105,13 @@ try:
 
     if challengeLevel == 1:
         while rclpy.ok():
-            rclpy.spin_once(robot, timeout_sec=0.1)
-            time.sleep(0.1)
-            # Write your solution here for challenge level 1
-            # It is recommended you use functions for aspects of the challenge that will be resused in later challenges
-            # For example, create a function that will detect if the robot is too close to a wall
-
+            print("level 1 start!")
+            auto_avoid_obstacle(lidar, control, robot)
+            
     if challengeLevel == 2:
-        print("level 2 start!")
+        print("level 2 start!") 
         handle_stop_sign_and_apriltag(camera, control, robot)
 
-            
     if challengeLevel == 3:
         while rclpy.ok():
             rclpy.spin_once(robot, timeout_sec=0.1)
